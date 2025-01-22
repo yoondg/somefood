f4642d50134d3f00cf102a639297a5062de102ad
yoondg <ekrud0927@gmail.com>
2025-01-22 10:49:16 +0900

    0122

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import SomeFood from './components/SomeFood';
-import MainVisual from './components/MainVisual';
-import NewsEvent from './components/NewsEvent';
 import HomePage from './pages/HomePage';
 import DetailPage from './pages/DetailPage';
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/components/Footer.css'
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
